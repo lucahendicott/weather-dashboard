3aac834a6fc5db841194b35730bf6bb2ac4d43b4
Lucah Endicott <lucahdavid88@gmail.com>
2020-11-07 12:00:54 -0800

    add readme

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,56 @@
 let apiKey = "30da43373157d45ad4440e9c0dc3b9d0"
 let cityName = "oakland"
+let lastCity = ""
 let fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey
-let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+let currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial"
 let currentCity = document.querySelector("#city")
 let currentCityTemp = document.querySelector("#temp")
 let currentCityHumid = document.querySelector("#humidity")
 let currentCityWind = document.querySelector("#windSpeed")
 let currentCityUv = document.querySelector("#uvIndex")
 
-fetchCurrentRequest()
-function fetchCurrentRequest() {
+fetchCurrent()
+fetchFiveDay()
+
+function fetchCurrent() {
 fetch(currentUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
-    console.log("city name:", data.name);
-    console.log("city temp:", data.main.temp);
-    console.log("city humidity:", data.main.humidity);
-    currentCity.innerHTML = data.name + new Date().toDateString()
-    currentCityTemp.innerHTML = "Temperature: " + data.main.temp
-  });
+    currentCity.innerHTML = data.name + ": " + new Date().toLocaleString() 
+    currentCityTemp.innerHTML = "Temperature: " + data.main.temp + " °F"
+    currentCityHumid.innerHTML = "Humidity: " + data.main.humidity + "%"
+    currentCityWind.innerHTML = "Wind Speed: " + data.wind.speed + " MPH"
+    currentCityUv.innerHTML = "UV Index: " + latitude + longitude
+    });
 };
 
-function fetchFiveDayRequest() {
+
+function fetchFiveDay() {
     fetch(fiveDayUrl)
       .then(function (response) {
         return response.json();
+        
       })
       .then(function (data) {
         console.log("five day", data);
+      
       });
     };
-
+    
+    let uvIndexUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + apiKey
+    let latitude = cityName.data.coord.lat
+    let longitude = cityName.data.coord.lon
+    
+    fetch(uvIndexUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("UV", data);
+      
+    });
+    
 
