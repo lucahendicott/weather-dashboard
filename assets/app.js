c2491f7eaacfce96ea30e7d5641e968c76df1e69
Lucah Endicott <lucahdavid88@gmail.com>
2020-11-10 17:58:07 -0800

    update 5 day cast function to send values to HTML

--- assets/app.js
+++ assets/app.js
@@ -7,13 +7,27 @@ let currentCityTemp = document.querySelector("#temp")
 let currentCityHumid = document.querySelector("#humidity")
 let currentCityWind = document.querySelector("#windSpeed")
 let currentCityUv = document.querySelector("#uvIndex")
-let dayOneDate = document.querySelector("#city5")
-let dayOneTemp = document.querySelector("#temp5")
-let dayOneHumid = document.querySelector("#humidity5")
+let dayOneDate = document.querySelector("#cityOne5")
+let dayOneTemp = document.querySelector("#tempOne5")
+let dayOneHumid = document.querySelector("#humidityOne5")
+let dayTwoDate = document.querySelector("#cityTwo5")
+let dayTwoTemp = document.querySelector("#tempTwo5")
+let dayTwoHumid = document.querySelector("#humidityTwo5")
+let dayThreeDate = document.querySelector("#cityThree5")
+let dayThreeTemp = document.querySelector("#tempThree5")
+let dayThreeHumid = document.querySelector("#humidityThree5")
+let dayFourDate = document.querySelector("#cityFour5")
+let dayFourTemp = document.querySelector("#tempFour5")
+let dayFourHumid = document.querySelector("#humidityFour5")
+let dayFiveDate = document.querySelector("#cityFive5")
+let dayFiveTemp = document.querySelector("#tempFive5")
+let dayFiveHumid = document.querySelector("#humidityFive5")
+
 
 fetchCurrent()
 fetchFiveDay()
 
+// function fetching the current day weather and appending those values to the HTML content
 function fetchCurrent(cityValue) {
   if(!cityValue){
     cityValue = "oakland"
@@ -33,14 +47,14 @@ fetch(currentUrl)
     });
 };
 
+// add event listener to the search button and display the values for the fetch functions called inside
  submitCityInput.addEventListener("click", function(e){
    e.preventDefault()
-  // lastCity = cityNameInput.value
   fetchCurrent(cityNameInput.value)
   fetchFiveDay(cityNameInput.value)
  })
 
-
+// function fetching 5 day url and appending the values to the HTML content
 function fetchFiveDay(cityValue) {
   if(!cityValue){
     cityValue = "oakland"
@@ -54,8 +68,20 @@ function fetchFiveDay(cityValue) {
       .then(function (data) {
         console.log("five day", data);
         dayOneDate.innerHTML = new Date(data.list[4].dt_txt).toLocaleString() 
-        dayOneTemp.innerHTML = data.list[4].main.temp + " °F"
-        dayOneHumid.innerHTML = data.list[4].main.humidity
+        dayOneTemp.innerHTML = "Temerature: " + data.list[4].main.temp + " °F"
+        dayOneHumid.innerHTML = "Humidity: " + data.list[4].main.humidity + "%"
+        dayTwoDate.innerHTML = new Date(data.list[12].dt_txt).toLocaleString() 
+        dayTwoTemp.innerHTML = "Temerature: " + data.list[12].main.temp + " °F"
+        dayTwoHumid.innerHTML = "Humidity: " + data.list[12].main.humidity + "%"
+        dayThreeDate.innerHTML = new Date(data.list[20].dt_txt).toLocaleString() 
+        dayThreeTemp.innerHTML = "Temerature: " + data.list[20].main.temp + " °F"
+        dayThreeHumid.innerHTML = "Humidity: " + data.list[20].main.humidity + "%"
+        dayFourDate.innerHTML = new Date(data.list[28].dt_txt).toLocaleString() 
+        dayFourTemp.innerHTML = "Temerature: " + data.list[28].main.temp + " °F"
+        dayFourHumid.innerHTML = "Humidity: " + data.list[28].main.humidity + "%"
+        dayFiveDate.innerHTML = new Date(data.list[36].dt_txt).toLocaleString() 
+        dayFiveTemp.innerHTML = "Temerature: " + data.list[36].main.temp + " °F"
+        dayFiveHumid.innerHTML = "Humidity: " + data.list[36].main.humidity + "%"
       });
     };
     
